4c6225bbdd26e7e19e396e29c4f5f9bad60894ea
ahatwar822 <himanshugaupale875@gmail.com>
2025-12-27 22:16:32 +0530

    final commit for favroite

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import RecipeCard from './RecipeCard/RecipeCard'
 
 const Fav = () => {
+  const favroite = JSON.parse(localStorage.getItem('fav'))
+
+  const randerRecipes = favroite.map((recipe) => {
+    return <RecipeCard key={recipe.id} recipe={recipe} />
+  })
+
   return (
-    <div>Fav</div>
+    <>
+      <div className='flex flex-wrap gap-6 mt-9  '>{randerRecipes}</div>
+    </>
+
   )
 }
 
